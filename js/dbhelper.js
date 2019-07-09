38f2fb454a849fd15b6610225b8c8092a20df6a7
rowa-codes <raowaam@gmail.com>
2019-07-09 13:45:44 +0300

    Update dbhelper.js
    .

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -150,7 +150,7 @@ class DBHelper {
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return (`/restaurant-reviewer-udacity/img/${restaurant.photograph}`);
+    return (`/restaurant-reviewer-udacit/img/${restaurant.photograph}`);
   }
 
   /**
